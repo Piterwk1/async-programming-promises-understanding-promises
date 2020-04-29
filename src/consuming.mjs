595df75db22c8e7c5e9cb3a885488b20e90f07cb
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-29 22:51:52 +0200

    add chain

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,35 +1,33 @@
-import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+import setText, { appendText, showWaiting, hideWaiting } from './results.mjs';
 
-export function get(){
-    
-    axios.get("http://localhost:3000/orders/1")
-    .then(({data}) => {
-        setText(JSON.stringify(data));
-    });
-    
+export function get() {
+  axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
-export function getCatch(){
-    axios.get("http://localhost:3000/orders/123")
-    .then(({data}) => {
-        setText(JSON.stringify(data));
+export function getCatch() {
+  axios
+    .get('http://localhost:3000/orders/123')
+    .then(({ data }) => {
+      setText(JSON.stringify(data));
     })
     .catch(err => setText(err));
-
-    // axios.get("http://localhost:3000/orders/123").then((result) => {
-    //     if (result.status === 200) {
-    //         setText(JSON.stringify(result.data));
-    //     } else {
-    //         setText("Error");
-    //     }
-       
-    // });
 }
 
-export function chain(){
+export function chain() {
+  axios.get("http://localhost:3000/orders/1")
+  .then(({
+      data
+  }) => {
+      return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+  })
+  .then(({
+      data
+  }) => {
+      setText(`City: ${data.city}`);
+  })
 }
 
-export function chainCatch(){
-}
-export function final(){
-}
+export function chainCatch() {}
+export function final() {}
