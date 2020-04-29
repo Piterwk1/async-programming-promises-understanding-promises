8f4f655d4760850e2d652fc280c87c64008ddc86
Piotr Kaminski <piterwk1@.wp.pl>
2020-04-29 12:20:57 +0200

    initial commit

---
+++ .history/src/consuming_20200429110911.mjs
@@ -0,0 +1,16 @@
+import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+
+export function get(){
+    axios.get("http://localhost:3000/orders/1");
+}
+
+export function getCatch(){
+}
+
+export function chain(){
+}
+
+export function chainCatch(){
+}
+export function final(){
+}
---
+++ src/consuming.mjs
@@ -0,0 +1,17 @@
+import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+
+export function get(){
+    
+    axios.get("http://localhost:3000/orders/1")
+}
+
+export function getCatch(){
+}
+
+export function chain(){
+}
+
+export function chainCatch(){
+}
+export function final(){
+}
